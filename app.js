fb0c2c2e1dd28ff8762b9a8be98237eba1462ff0
Hunter Hamilton <iloveyouexe1@gmail.com>
2022-03-07 11:14:57 -0600

    final commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// let p1 = {
+//   name: "Hunter",
+//   sayHello: function () {
+//     alert(`Hi! I am ${this.name}.`);
+//   },
+// };
+
+// let p2 = {
+//   name: "Christoph",
+//   sayHello: function () {
+//     alert(`Hi, I am ${this.name}.`);
+//   },
+// };
+
+// p1.sayHello();
+// p2.sayHello();
+
+// This is the Pseudo-Class method.
+//  constructor
+function Person(name, age) {
+  //  set properties using
+  //  this.propName syntax
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype.sayHello = function () {
+  // this is storing a function with the name
+  // "sayHello" to the "Person" function above.
+  // function Person, name and age, function sayHello.
+  alert("Hello my name is " + this.name + "! I am " + this.age + " years old.");
+};
+
+// let p1 = new Person("Hunter", 25);
+// p1.sayHello();
+
+// Creating Object Instances
+
+let p1 = new Person("Jane", 29);
+let p2 = new Person("John", 28);
+p1.sayHello();
+p2.sayHello();
